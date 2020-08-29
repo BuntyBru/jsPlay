5a23612e30cee19d5e58086acc25adeb08882f9d
divyanshuStrata <divyanshu@strataprop.com>
2020-08-29 21:38:46 +0530

    functionalities added

--- app.js
+++ app.js
@@ -1,5 +1,71 @@
-var titles = document.getElementsByClassName('title')
+const list = document.querySelector('#book-list ul');
 
-Array.from(titles).forEach((x)=>{
-	console.log(x);
+
+//deletion 
+list.addEventListener('click',(e)=>{
+
+	if(e.target.className == 'delete')
+	{
+		console.log("Delete",e.target.parentNode);
+list.removeChild(e.target.parentNode)
+
+	}
 })
+
+
+//forms adding books
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit',(e)=>{
+	e.preventDefault();
+
+	const val  = addForm.querySelector("input[type='text']").value;
+
+	
+	if(val.length>0)
+	{
+			const li = document.createElement('li');
+	const bookName = document.createElement('span');
+	bookName.textContent = val;
+	bookName.classList.add('name');
+
+	const delBtn =document.createElement('span');
+	delBtn.textContent='delete';
+	delBtn.classList.add('delete');
+
+
+	li.appendChild(bookName);
+	li.appendChild(delBtn);
+	li.setAttribute('data-id',val)
+	list.appendChild(li)
+	addForm.querySelector("input[type='text']").value=''
+
+	}
+
+})
+
+
+//search bar
+
+const searchForm = document.forms['search-books'];
+searchForm.addEventListener('keyup',(e)=>{
+	const term = e.target.value.toLowerCase();
+
+	let books  = document.querySelectorAll('li');
+	Array.from(books).forEach( book => {
+		const title = book.firstElementChild.textContent;
+		if(title.toLowerCase().indexOf(term) !=-1)
+		{
+			book.style.display = 'block';
+
+		}
+		else
+		{
+			book.style.display = 'none';
+		}
+	})
+
+})
+
+
